b53e0ee6f6b4d9aa53e12278b058835bc033e366
tristan <jametri014@gmail.com>
2022-08-12 16:45:47 -0400

    Wrote out hamletParser

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.time.Period;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,6 +11,15 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    private final Pattern hamletPattern = Pattern.compile("Hamlet");
+    private final Pattern bigHam = Pattern.compile("HAMLET");
+    private final Pattern horatioPattern = Pattern.compile("Horatio");
+    private final Pattern bigHor = Pattern.compile("HORATIO");
+    private final String hamTris = "Tristan";
+    private final String bigTris = "TRISTAN";
+    private final String horaTariq = "Tariq";
+    private final String bigTariq = "TARIQ";
+    private Matcher matcher;
 
     public HamletParser(){
         this.hamletData = loadFile();
@@ -33,6 +45,38 @@ public class HamletParser {
     }
 
     public String getHamletData(){
+        return hamletData;
+    }
+
+    public String hamletToTris(String data) {
+        matcher = hamletPattern.matcher(data);
+        hamletData = matcher.replaceAll(hamTris);
+        return hamletData;
+    }
+
+    public String horatioToTariq(String data) {
+        matcher = horatioPattern.matcher(data);
+        hamletData = matcher.replaceAll(horaTariq);
+        return hamletData;
+    }
+
+    public boolean findHoratio(String data) {
+        return horatioPattern.matcher(data).find();
+    }
+
+    public boolean findHamlet(String data) {
+        return hamletPattern.matcher(data).find();
+    }
+
+    public String bigHamChanger(String data) {
+        matcher = bigHam.matcher(data);
+        hamletData = matcher.replaceAll(bigTris);
+        return hamletData;
+    }
+
+    public String bigHorChanger(String data) {
+        matcher = bigHor.matcher(data);
+        hamletData = matcher.replaceAll(bigTariq);
         return hamletData;
     }
 
