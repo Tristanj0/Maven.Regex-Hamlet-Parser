a327ee6137e8f30fae618356674502b85e4fb9dc
tristan <jametri014@gmail.com>
2022-08-12 16:59:56 -0400

    Wrote hamletParserTest and used replaceAll

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,18 +15,45 @@ public class HamletParserTest {
     }
 
     @Test
-    public void testChangeHamletToLeon() {
+    public void testChangeHamletToTris() {
+        String expected = "My name is Tris";
+        String input = "My name is Hamlet";
+
+        String actual = hamletParser.hamletToTris(input);
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String expected = "My name is Tariq";
+        String input = "My name is Horatio";
+
+        String actual = hamletParser.horatioToTariq(input);
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
+        String input = "My name is Horatio";
+
+        Assert.assertTrue(hamletParser.findHoratio(input));
     }
 
     @Test
     public void testFindHamlet() {
+        String input = "My name is Hamlet";
+
+        Assert.assertTrue(hamletParser.findHamlet(input));
+    }
+
+    @Test
+    public void hamletParserTest() {
+        String hamToTris = hamletParser.hamletToTris(hamletText);
+        String horToTariq = hamletParser.horatioToTariq(hamToTris);
+        String bigHam = hamletParser.bigHamChanger(horToTariq);
+        String bigTor = hamletParser.bigHorChanger(bigHam);
+        System.out.println(bigTor);
     }
 }
